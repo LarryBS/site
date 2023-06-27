ab28cfae84660b4cc941125c9dff28020654b47a
DevOps <alexeymihaylovdev@gmail.com>
2023-06-27 22:57:26 +0300

    12

--- src/App.js
+++ src/App.js
@@ -18,6 +18,15 @@ const CategoryPanel = () => {
 const App = () => {
   return (
     <div className="App">
+      <div className="logo-container">
+        <a href="https://ali-buy.com" className="logo-image">
+          <img
+            src="https://ali-buy.com/wp-content/uploads/2021/10/alibuy-logo-trans.png"
+            alt="AliBuy"
+            width="200"
+          />
+        </a>
+      </div>
       <CategoryPanel />
       <h1>My Search App</h1>
       <SearchBar />
